64195785001845f2172120d39457b6fb592fe59e
ibxgolden <129530843+ibxgolden@users.noreply.github.com>
2023-05-17 11:42:16 +0300

    Update abc.js
    when you click on submit it will show off the results

--- abc.js
+++ abc.js
@@ -104,3 +104,28 @@ function showQuestions(questions, quizContainer){
 
 	quizContainer.innerHTML = output.join('');
 }
+function showResults(questions, quizContainer, resultsContainer){
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	for(var i=0; i<questions.length; i++){
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+	
+		if(userAnswer===questions[i].correctAnswer){
+			
+			numCorrect++;
+			answerContainers[i].style.color = 'lightgreen';
+		}
+		
+		else{
+		
+			answerContainers[i].style.color = 'red';
+		}
+	}
+
+	// show number of correct answers out of total
+	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
