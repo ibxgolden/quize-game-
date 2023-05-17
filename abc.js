5aff5c44ff0e81f5de5793e7adcc40847f1e658f
ibxgolden <129530843+ibxgolden@users.noreply.github.com>
2023-05-17 11:37:06 +0300

    Update abc.js
    For each question, show the question along with all of its answer choices.

--- abc.js
+++ abc.js
@@ -75,3 +75,32 @@ var myQuestions = [
 		correctAnswer: 'a'
 	}
 ];
+function showQuestions(questions, quizContainer){
+	var output = [];
+	var answers;
+	for(var i=0; i<questions.length; i++){
+		
+	
+		answers = [];
+
+		for(letter in questions[i].answers){
+
+		
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+	
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	quizContainer.innerHTML = output.join('');
+}
